b1a17f02d8823bdc451688f4f1f5e40dc388e67e
mychidarko <mickdd22@gmail.com>
2024-11-13 02:17:17 +0000

    feat: add button component

--- src/Core.php
+++ src/Core.php
@@ -10,5 +10,171 @@ namespace Zero;
  */
 class Core
 {
-  //
+  protected static $imports = [];
+
+  protected static $styles = [];
+
+  protected static $bodyScripts = [];
+
+  protected static $globalMarkup = [];
+
+  /**
+   * Get zero head imports
+   */
+  public static function imports()
+  {
+
+    $imports = implode("\n", static::$imports);
+    $styles = implode("\n", static::$styles);
+
+    return <<<HTML
+    $imports
+    $styles
+    HTML;
+  }
+
+  /**
+   * Create a new element
+   * @param string $element
+   * @param array $props
+   * @param mixed $children
+   * @return string
+   */
+  public static function createElement(string $element, array $props = [], $children = [])
+  {
+    $subs = '';
+    $attributes = '';
+
+    if (isset($props['children']) && (!$children || ($children && empty($children)))) {
+      $children = $props['children'];
+      unset($props['children']);
+    }
+
+    if (is_array($children)) {
+      foreach ($children as $child) {
+        $subs .= $child;
+      }
+    } else {
+      $subs = $children;
+    }
+
+    if (!empty($props)) {
+      foreach ($props as $key => $value) {
+        $attributes .= "$key=\"" . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . "\" ";
+      }
+    }
+
+    return (!$children || $children && empty($children))
+      ? "<$element $attributes />"
+      : "<$element $attributes>$subs</$element>";
+  }
+
+  /**
+   * Map scripts to script tag
+   *
+   * @param array $scripts The scripts to apply
+   * @param array $props Script tag attributes
+   */
+
+  /**
+   * Map styles to style tag
+   *
+   * @param array $styles The styles to apply
+   * @param array $props Style tag attributes
+   */
+  public static function createStyles(array $styles, array $props = [])
+  {
+    return self::createElement(
+      'style',
+      $props,
+      Core::parseStyles($styles)
+    );
+  }
+
+  /**
+   * Loop over an array of items
+   *
+   * @param array|string|int $array The array to loop through
+   * @param callable $handler Call back function to run per iteration
+   */
+  public static function loop($array, callable $handler)
+  {
+    $element = '';
+
+    if (!is_array($array)) {
+      $array = explode(',', str_repeat(',', (int) $array - 1));
+    }
+
+    if (is_callable($handler)) {
+      foreach ($array as $key => $value) {
+        $element .= call_user_func($handler, $value, $key);
+      }
+    }
+
+    return $element;
+  }
+
+  /**
+   * Merge tailwind styles
+   */
+  public static function mergeStyles(...$styles)
+  {
+    $mergedStyles = [];
+
+    foreach ($styles as $style) {
+      $style = explode(' ', $style);
+      $mergedStyles = array_merge($mergedStyles, $style);
+    }
+
+    return implode(' ', $mergedStyles);
+  }
+
+
+  protected static function parseStyles(array $styles): string
+  {
+    $parsedStyles = '';
+
+    foreach ($styles as $key => $value) {
+      if (is_numeric($key)) {
+        $value = rtrim($value, ';');
+        $parsedStyles .= "$value;";
+      } else if (is_string($value)) {
+        $value = rtrim($value, ';');
+
+        if (strpos($value, ':') !== false) {
+          $parsedStyles .= "$key { $value; }";
+        } else {
+          $parsedStyles .= "$key: $value;";
+        }
+      } else {
+        $parsedStyles .= "$key {";
+
+        foreach ($value as $selector => $styling) {
+          if (is_array($styling)) {
+            if (is_string($selector)) {
+              $parsedStyles .= self::parseStyles([$selector => $styling]);
+            } else {
+              $parsedStyles .= self::parseStyles($styling);
+            }
+          } else {
+            $styling = rtrim($styling, ';');
+
+            if (is_numeric($selector)) {
+              $parsedStyles .= self::parseStyles(["$styling;"]);
+            } else {
+              if (strpos($styling, ':') !== false) {
+                $parsedStyles .= "$selector { $styling; }";
+              } else {
+                $parsedStyles .= "$selector: $styling;";
+              }
+            }
+          }
+        }
+
+        $parsedStyles .= '}';
+      }
+    }
+
+    return $parsedStyles;
+  }
 }
---
+++ src/components/button.php
@@ -0,0 +1,45 @@
+<?php
+
+use Zero\Core;
+
+function Button(array $props)
+{
+  $as = $props['as'] ?? 'button';
+  $props['type'] = $props['type'] ?? 'button';
+  $props['data-zero-component'] = 'Button';
+
+  if (isset($props['href'])) {
+    $as = 'a';
+  }
+
+  $defaultStyles = 'transition-all inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4';
+  $buttonStyles = [
+    'text' => '',
+    'link' => 'text-[#EC504B] hover:text-[#FFAA49]',
+    'default' => 'bg-[#EC504B] text-white hover:bg-[#FF6A49]',
+    'outline' => 'bg-white/0 text-slate-900 ring-1 ring-slate-900/10 hover:bg-white/25 hover:ring-slate-900/15 whitespace-nowrap'
+  ];
+
+  $props['class'] = Core::mergeStyles(
+    $defaultStyles,
+    $buttonStyles[$props['variant'] ?? 'default'],
+    $props['class'] ?? ''
+  );
+
+  if ($as === 'a') {
+    unset($props['type']);
+    unset($props['as']);
+  }
+
+  if (isset($props['icon'])) {
+    $children[] = Core::createElement(
+      'span',
+      ['class' => 'ml-2', 'aria-hidden' => 'true'],
+      $props['icon']
+    );
+
+    unset($props['icon']);
+  }
+
+  return Core::createElement($as, $props, $children);
+}
